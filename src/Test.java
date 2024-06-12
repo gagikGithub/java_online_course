1fbfcd27b53ab49e0bc59ccc7b71c48edfd43ce4
Gagik <gagiknaghashyan@gmail.com>
2024-06-13 02:06:34 +0400

    first commit

---
+++ src/Test.java
@@ -0,0 +1,51 @@
+public class Test {
+    public static void main(String[] args) {
+        // java code test from Lesson 1
+
+        System.out.println("Hello World from Java code");
+        System.out.print("Java lesson\n");
+        System.out.println("Code Example");
+
+        // primitive data types
+
+        byte x = 127;
+        System.out.println(x);
+
+        short y = 4543;
+        System.out.println(y);
+
+        int a = 77821;
+        System.out.println(a);
+
+        long b = 38727222L;
+        System.out.println(b);
+
+        float f = 4.54f;
+        System.out.println(f);
+        double d = 4.45;
+        System.out.println(d);
+
+        boolean l = true;
+        System.out.println(l);
+        l = false;
+        System.out.println(l);
+
+        char t = 't';
+        System.out.println(t);
+
+        char tt = '\u0040';
+        System.out.println(tt);
+
+        String nameOfUser = "Bob";
+        System.out.println(nameOfUser);
+
+        String secondNameUser = "Doe";
+        System.out.println(secondNameUser);
+
+        System.out.println(nameOfUser + " " + secondNameUser);
+
+        int itemNumber = 100;
+
+        System.out.println("itemNumber = " + itemNumber);
+    }
+}
